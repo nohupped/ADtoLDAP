aadafce57550dbc48759af2030d208c890285cda
Girish <nohupped@gmail.com>
2016-09-26 13:59:36 +0530

    Removed debug logging, changed modules to github link

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"os/user"
 	"fmt"
 	"os"
-	"ADtoLDAP/gosyncmodules"
-//	"github.com/nohupped/ADtoLDAP/gosyncmodules"
+//	"ADtoLDAP/gosyncmodules"
+	"github.com/nohupped/ADtoLDAP/gosyncmodules"
 	"reflect"
 //	"os/signal"
 	"github.com/nohupped/ldap" //using a forked version that includes custom methods to retrieve and edit *AddRequest struct.
@@ -203,16 +203,16 @@ func main() {
 			for ; ; {
 				select {
 				case Add := <- AddChan:
-					for k, v := range Add {
-						gosyncmodules.Info.Println(k, ":", v)
+					for _, v := range Add {
+						//gosyncmodules.Info.Println(k, ":", v)
 						err := LDAPConnection.Add(v)
 						if err != nil {
 							gosyncmodules.Error.Println(err)
 						}
 					}
 				case Del := <- DelChan:
-					for k, v := range Del  {
-						gosyncmodules.Info.Println(k, ":", v)
+					for _, v := range Del  {
+						//gosyncmodules.Info.Println(k, ":", v)
 						delete := ldap.NewDelRequest(v.DN, []ldap.Control{})
 						err := LDAPConnection.Del(delete)
 						if err != nil {
