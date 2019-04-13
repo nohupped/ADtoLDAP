a9ac3dcb16ab51d947baf4fe5e1c99cb6d43c0fc
girish_arch <nohupped@gmail.com>
2019-04-13 22:19:13 +0800

    use constants defined in syscall package

--- gosyncmodules/CheckPerm.go
+++ gosyncmodules/CheckPerm.go
@@ -8,17 +8,6 @@ import (
 	"syscall"
 )
 
-// Constants S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH and S_IXOTH values are taken straight from /stat.h.
-// Ref: https://github.com/torvalds/linux/blob/master/include/uapi/linux/stat.h
-const (
-	IRGRP = 0000040
-	IWGRP = 0000020
-	IXGRP = 0000010
-	IROTH = 0000004
-	IWOTH = 0000002
-	IXOTH = 0000001
-)
-
 // CheckPerm checks permission of the passed filename(string) and panics if group or others have > read permissions.
 func CheckPerm(filename string) {
 	currentUID := os.Getuid()
@@ -49,12 +38,11 @@ func CheckPerm(filename string) {
 		logger.Errorf("%s not owned by uid=%d(%s) or gid=%d(%s). Do chown %d:%d %s\n", filename, currentUID, currentUserName, currentGID, currentGroupName, currentUID, currentGID, filename)
 		os.Exit(1)
 	}
-	if filestatSys.Mode&IRGRP > 0 || filestatSys.Mode&IWGRP > 0 || filestatSys.Mode&IXGRP > 0 ||
-	   filestatSys.Mode&IROTH > 0 || filestatSys.Mode&IWOTH > 0 || filestatSys.Mode&IXOTH > 0 {
+	if filestatSys.Mode&syscall.S_IRGRP > 0 || filestatSys.Mode&syscall.S_IWGRP > 0 || filestatSys.Mode&syscall.S_IXGRP > 0 ||
+		filestatSys.Mode&syscall.S_IROTH > 0 || filestatSys.Mode&syscall.S_IWOTH > 0 || filestatSys.Mode&syscall.S_IXOTH > 0 {
 		fmt.Println(filename, "file permission too broad, make it non-readable to groups and others.")
 		logger.Errorln(filename, "file permission too broad, make it non-readable to groups and others.")
 		os.Exit(1)
 	}
 	logger.Infoln("File permission looks secure")
 }
-
